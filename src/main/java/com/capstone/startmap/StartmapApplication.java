4b95356b4f56444147904185b2565cb885b6e0f6
hyyeeon <gpdus8549@gmail.com>
2025-02-26 15:50:53 +0900

    Initial commit

---
+++ src/main/java/com/capstone/startmap/StartmapApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.startmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/startmap/StartmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.startmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
