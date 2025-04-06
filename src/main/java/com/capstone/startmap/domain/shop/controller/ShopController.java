c8995e68c6f05326a5fadff35f8c7f0d8a3e0889
hyyeeon <gpdus8549@gmail.com>
2025-04-06 21:45:21 +0900

    shopResDto 및 shopcontroller 프레임

--- src/main/java/com/capstone/startmap/domain/shop/controller/ShopController.java
+++ src/main/java/com/capstone/startmap/domain/shop/controller/ShopController.java
@@ -1,5 +1,6 @@
 package com.capstone.startmap.domain.shop.controller;
 
+import com.capstone.startmap.domain.shop.api.dto.ShopResDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class ShopController {
-    public Shop showShop(Long )
+    public ShopResDto showShop(Long shop_id) {
+        return null;
+    }
 
 }
+
