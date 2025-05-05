4db73428edda0b9c23bdf62950147e02c3adcfc2
hyyeeon <gpdus8549@gmail.com>
2025-05-06 00:21:32 +0900

    Town resdto, controller, repository, service

---
+++ src/main/java/com/capstone/startmap/exception/town/NotFoundTownException.java
@@ -0,0 +1,10 @@
+package com.capstone.startmap.exception.town;
+
+public class NotFoundTownException extends RuntimeException {
+  public NotFoundTownException(final String message) {
+    super(message);
+  }
+  public NotFoundTownException() {
+    this("동을 찾을 수 없습니다.");
+  }
+}
